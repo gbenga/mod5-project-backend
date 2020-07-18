af4f4f88a96453ef71aea3f6ef617d8c79ac46d4
gojoaromokudu <gojoaromokudu@gmail.com>
2020-07-18 17:34:56 +0100

    Create OrderMedicine controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:index, :show]
       resources :orders, only: [:index, :show, :new, :create]
+      resources :order_medicines, only: [:index, :show, :new, :create]
       resources :medicines, only: [:index, :show]
       resources :pharmas, only: [:index, :show]
       resources :stocks, only: [:show]
