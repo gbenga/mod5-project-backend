210e75a3ba02adad3ff4105352542cd42d293019
gojoaromokudu <gojoaromokudu@gmail.com>
2020-07-20 17:18:45 +0100

    Add jwt gem, validate route and action, and decode action to application controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -20,7 +20,22 @@ class Api::V1::UsersController < ApplicationController
         #If we can find them, try and authenticate that user with the password we've been given
         if user && user.authenticate(params[:password])
             # If it was successful, log them in
-            render json: {user: user}
+            render json: {user: user, token: generate_token({id: user.id})}
+        else
+            #Otherwise, send back an authentication error
+            render json: {message: "Failure"}
+        end
+    end
+
+    def validate
+        #Decode the token that it's been sent as the Authorization header in order to get the id of the logged in user
+        id = decode_token
+        #Use that id to find the logged in user
+        user = User.find_by(id: id)
+        #Respond with the user object of the logged in user and a new token from their id
+        if user 
+            # If it was successful, log them in
+            render json: {user: user, token: generate_token({id: user.id})}
         else
             #Otherwise, send back an authentication error
             render json: {message: "Failure"}
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+
+    def secret
+        ENV["MY_SECRET"]
+    end
+
+    def generate_token(data)
+        JWT.encode(data, secret)
+    end
+    
+    def decode_token
+        token = request.headers["Authorization"]
+        JWT.decode(token, secret).first["id"]
+    end
 end
