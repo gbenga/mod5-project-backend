8041932053a8901ac857d83c449a8ab50d47f889
gojoaromokudu <gojoaromokudu@gmail.com>
2020-07-28 21:57:25 +0100

    Render full error messages in JSON

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::UsersController < ApplicationController
+    skip_before_action :authorised?, only: [:create, :sign_in]
+    
     def index
         users = User.all
         render json: users, include: [:orders, :medicines]
     end
 
     def show
-        user = User.find_by(id: params[:id])
+        user = logged_in_user
         if user
             render json: user, include: [:orders, :medicines]
         else
@@ -20,28 +22,40 @@ class Api::V1::UsersController < ApplicationController
     def create
         user = User.new(user_params)
         user.password = params[:password]
-        if user.valid? 
+        if user.valid? && user.valid_password?
             user.save
+            render json: user, include: [:orders, :medicines]
+        else
+            errors = user.errors.full_messages
+            errors.push("Password is too short") if !user.valid_password?
+            render json: {errors: user.errors.full_messages}
+        end
+    end
+
+    def edit
+        user = logged_in_user
+    end
+
+    def update
+        user = logged_in_user
+
+        user.update(user_params)
+        if user.valid?
             render json: user, include: [:orders, :medicines]
         else
             render json: {errors: user.errors.full_messages}
         end
     end
 
-    def edit
-        user = User.find_by(id: params[:id])
-    end
+    def change_password
+        user = logged_in_user
+        user.password = params[:new_password]
 
-    def update
-        user = User.find_by(id: params[:id])
-        # user.password = params[:password]
-        byebug
-        user.update(user_params)
-        byebug
-        if user.valid?
-            render json: user, include: [:orders, :medicines]
+        if user.authenticate(params[:current_password]) && user.valid_password?
+            user.save
+            render json: { message: "Password has been updated!" }
         else
-            render json: {errors: user.errors.full_messages}
+            render json: { errors: ["Password is too short"]}
         end
     end
 
@@ -55,15 +69,15 @@ class Api::V1::UsersController < ApplicationController
             render json: {user: user, token: generate_token({id: user.id})}
         else
             #Otherwise, send back an authentication error
-            render json: {message: "Failure"}
+            render json: {errors: "You could not be signed in. Check your username and password combination"}
         end
     end
 
     def validate
         #Decode the token that it's been sent as the Authorization header in order to get the id of the logged in user
-        id = decode_token
+        # id = decode_token
         #Use that id to find the logged in user
-        user = User.find_by(id: id)
+        user = logged_in_user
         #Respond with the user object of the logged in user and a new token from their id
         if user 
             # If it was successful, log them in
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,16 @@ class User < ApplicationRecord
     has_many :stocks, through: :medicines
     has_many :pharmas, through: :stocks
 
-    validates :first_name, :last_name, :phone, :address, :dob, :sex, :username, :password, presence: true
+    validates :first_name, :last_name, :phone, :address, :dob, :sex, :username, presence: true
     validates :username, length: { in: 2..20 }
     validates :username, uniqueness: { case_sensitive: false }
     
-    validates :password, length: { in: 6..20 }
+    # validates :password, length: { in: 6..20 }
     
     validates :phone, length: { is: 11, wrong_length: "Phone numbers in the UK are exactly 11 numbers long. Where are you from?" }
     validates :phone, numericality: { message: "%{value} isn't a number..." }
+
+    def valid_password?
+        self.password.length > 6
+    end
 end
