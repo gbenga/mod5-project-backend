2d607b5c529391358820969f0d59a665d908ad91
gojoaromokudu <gojoaromokudu@gmail.com>
2020-07-29 19:25:10 +0100

    Add local vs production environment detection to cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,9 +5,11 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
+frontend_origin = ENV['RAILS_ENV'] === "production" ? 'https://relaxed-leavitt-596441.netlify.app' : 'http://localhost:3001'
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://relaxed-leavitt-596441.netlify.app'
+    origins frontend_origin
 
     resource '*',
       headers: :any,
